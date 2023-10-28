454933c57502488c317866c0adfa4d803c9108b2
ElizaZalikha <149262981+ElizaZalikha@users.noreply.github.com>
2023-10-29 01:45:59 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var x = document.getElementById("myRadio").required;
+    document.getElementById("demo").innerHTML = x;
+  }
+datePickerId.max = new Date().toISOString().split("T")[0];
